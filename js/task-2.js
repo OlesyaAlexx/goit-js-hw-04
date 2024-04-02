f092e4772f841e3fe7ac971a8de9aad0f72dc20e
OlesyaAlexx <152548843+OlesyaAlexx@users.noreply.github.com>
2024-04-02 21:40:40 +0300

    mod after mentor

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,12 @@
 function calcAverageCalories(days) {
   let totalCalories = 0;
 
-  for (const day of days) {
-    totalCalories += day.calories;
-  }
-
   if (days.length === 0) {
     return 0;
+  }
+
+  for (const day of days) {
+    totalCalories += day.calories;
   }
 
   const averageCalories = totalCalories / days.length;
